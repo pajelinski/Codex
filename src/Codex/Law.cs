06fff384f01629c8c3a91594463218efc5e0ca4a
Palmetto <jelinski89@gmail.com>
2018-06-17 14:56:07 +0200

    implemented Law

---
+++ src/Codex/Law.cs
@@ -0,0 +1,24 @@
+﻿namespace Codex
+{
+    using System;
+    using Result;
+
+    public class Law<T>
+    {
+        private readonly Predicate<T> _predicate;
+        private readonly string _errorMessage;
+
+        public Law(Predicate<T> predicate, string errorMessage)
+        {
+            _predicate = predicate ?? throw new ArgumentNullException(nameof(predicate));
+            _errorMessage = errorMessage ?? throw new ArgumentNullException(nameof(errorMessage));
+        }
+
+        public IResult<Nothing> Evaluate(T subject)
+        {
+            return _predicate(subject) 
+                ? ResultFactory.CreateSuccess() 
+                : ResultFactory.CreateError(_errorMessage);
+        }
+    }
+}
---
+++ test/Codex.Tests/LawTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Codex.Tests
+{
+    using System;
+    using Result;
+    using Xunit;
+
+    public class LawTests
+    {
+        private readonly Nothing _nothing = new Nothing();
+        private readonly Predicate<Nothing> _falsePredicate = x => false;
+        private readonly Law<string> _isNotNullLaw;
+
+        public LawTests()
+        {
+            _isNotNullLaw = new Law<string>(x => !string.IsNullOrEmpty(x), string.Empty);
+
+        }
+
+        [Fact]
+        public void Evaluate_GivenFailingPredicate_ReturnsErrorMessage()
+        {
+            const string expectedErrorMessage = "errorMessage";
+            var result = new Law<Nothing>(_falsePredicate, expectedErrorMessage).Evaluate(_nothing);
+            Assert.Equal(expectedErrorMessage, result.GetError());
+        }
+
+        [Fact]
+        public void Evaluate_GivenIsNotNullPredicate_WhenEvaluatedValueIsNull_ReturnsError() => 
+            Assert.False(_isNotNullLaw.Evaluate(string.Empty).IsSuccess());
+
+        [Fact]
+        public void Evaluate_GivenIsNotNullPredicate_WhenEvaluatedValueIsNotNull_ReturnsSuccess() => 
+            Assert.True(_isNotNullLaw.Evaluate("string").IsSuccess());
+
+        [Fact]
+        public void WhenPredicateIsNull_ThrowsArgumentNullException() => 
+            Assert.Throws<ArgumentNullException>("predicate", () => new Law<Nothing>(null, string.Empty));
+
+        [Fact]
+        public void WhenErrorMessageIsNull_ThrowsArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>("errorMessage", () => new Law<Nothing>(_falsePredicate, null));
+    }
+}
